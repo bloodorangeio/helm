3c2b21ca2cb2aebcd93605af0fa7d1c6aa2a03c6
Josh Dolitsky <josh@dolit.ski>
2021-05-10 14:24:29 -0500

    Update to oras-go lib
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- pkg/registry/client.go
+++ pkg/registry/client.go
@@ -24,10 +24,10 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	auth "github.com/deislabs/oras/pkg/auth/docker"
-	"github.com/deislabs/oras/pkg/content"
-	"github.com/deislabs/oras/pkg/oras"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	auth "github.com/oras-project/oras-go/pkg/auth/docker"
+	"github.com/oras-project/oras-go/pkg/content"
+	"github.com/oras-project/oras-go/pkg/oras"
 	"github.com/pkg/errors"
 
 	"helm.sh/helm/v3/pkg/helmpath"
